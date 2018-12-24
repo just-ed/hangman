750c96e64fbd6c8b188ef7033599c755301ccf18
just-ed <46032349+just-ed@users.noreply.github.com>
2018-12-24 11:42:47 +0300

    Update word_reader.rb

--- word_reader.rb
+++ word_reader.rb
@@ -2,14 +2,8 @@
 class WordReader
   # Method for getting the word from the file
   def read_from_file(file_name)
-    # Open the file for reading
-    file = File.new(file_name, "r:UTF-8")
-    # Save every line from the file to an array
-    lines = file.readlines
-    # Close the file
-    file.close
-
-    # Return random word from the array
+    lines = File.readlines(file_name, "r:UTF-8")
+    
     lines.sample.downcase.chomp
   end
 end
