44bd5221dc839544114e627816ec86dc78ce5529
Edgar Tomeyan <46032349+just-ed@users.noreply.github.com>
2019-06-10 17:12:18 +0300

    hangman.rb refactored

--- hangman.rb
+++ hangman.rb
@@ -2,7 +2,7 @@ require_relative "lib/game"
 require_relative "lib/result_printer"
 require_relative "lib/word_reader"
 
-VERSION = "\"Hangman\" game. Version 5"
+VERSION = '"Hangman" game. Version 5'
 
 word_reader = WordReader.new
 words_file_name = "#{__dir__}/data/words.txt"
